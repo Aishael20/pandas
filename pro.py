a0afed216dc171f10fe77b31d6e11c8b4fc1e258
Aishael20 <aishadona10@gmail.com>
2024-05-22 12:00:02 -0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+# Création d'un tableau NumPy
+tableau = np.array([1, 2, 3, 4, 5])
+
+# Affichage du tableau
+print("Tableau NumPy :", tableau)
+
+# Quelques opérations avec NumPy
+somme = np.sum(tableau)
+moyenne = np.mean(tableau)
+
+print("Somme du tableau :", somme)
+print("Moyenne du tableau :", moyenne)
+
+    
